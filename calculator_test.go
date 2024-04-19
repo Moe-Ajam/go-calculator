39ccea19dd96e50025bf2395b949894a254f2a84
mahmoudajam <mahmoud.othman.ajam@gmail.com>
2024-04-19 23:59:33 +0300

    Provided test cases for the Add function

--- calculator_test.go
+++ calculator_test.go
@@ -8,10 +8,23 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+
+	testcases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 2, b: 3, want: 5},
+		{a: 4, b: 3, want: 7},
+	}
+
+	for _, tc := range testcases {
+		got := calculator.Add(tc.a, tc.b)
+		if got != tc.want {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
 	}
 }
 
